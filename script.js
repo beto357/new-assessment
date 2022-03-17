55c0caa2a514a4c1ca413022d928a6615362588f
beto357 <100175116+beto357@users.noreply.github.com>
2022-03-16 21:15:14 -0400

    Done TEST

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 // Obtain the img element, and assign it to the image variable
-let image
+let img = "dog"
 
-// Obtain the 'shrink-grow' button, and assign it to the changeSize variable
-let changeSize
+// Obtain the 'shrink-grow' button, and assign it to the changeSize variable 
+
+"shrink/grow" changeSize."dog"("click", function() 
+{
+   if (image.style.height == "300px") {
+    image.style.height = "150px"
+    image.style.width = "150px"
+  }
+  else {
+    image.style.height = "300px"
+    image.style.width = "300px"
+  }
+})
+
+
+
 
 //DO NOT CHANGE THE FOLLOWING CODE
 changeSize.addEventListener("click", function() {
